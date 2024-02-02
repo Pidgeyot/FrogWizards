01b64f83cea746b953800bef48939a3876aee755
Pidgeyot <pmabbitt@msn.com>
2024-02-01 21:40:50 -0700

    Initial Animations

--- Frog Wizards/Assets/_Scripts/Enemy.cs
+++ Frog Wizards/Assets/_Scripts/Enemy.cs
@@ -6,19 +6,52 @@ public class Enemy : MonoBehaviour
 {
     private int health = 5;
 
-    //if not in range of player
-        //wander around within a range of ____
-    //when player is in range
-        //follow around
-        //attack the player if close enough
-
-      void Update(){
-        if(health == 0){
+    void Update(){
+    
+      if(health == 0){
             //run dying anim
             Destroy(gameObject);
+      }else if (InRangeOfPlayer())
+      {
+        FollowPlayer();
+      }
+      else
+      {
+        Wander();
+      }
+    }
+
+    private bool InRangeOfPlayer()
+    {
+      // If enemy collider is in contact with player sphere
+      // Return true if in range, false otherwise
+      return false;
+    }
+
+    private void Wander()
+    {
+      // Implement wandering behavior within a specified range
+      //move in a small circle around point of origin
+    }
+
+    private void FollowPlayer()
+    {
+      //move towards the player
+       if (true) //colliding with player controller collider
+        {
+          AttackPlayer();
         }
-      }
-      public void hit(){
+    }
+
+    private void AttackPlayer()
+    {
+      //run attack anim
+      //if collliding with player/close enough
+      //deal damage to player
+    }
+
+    public void hit(){
         health --;
-      }
+    }
+       
 }
